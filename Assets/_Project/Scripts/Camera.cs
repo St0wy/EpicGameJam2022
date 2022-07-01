6938fe1843ca95a1e709d8bfccca3c83f1af178d
André <a.silva14@hotmail.com>
2022-07-01 22:21:04 +0200

    Camera and Health scripts to test

---
+++ Assets/_Project/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EpicGameJam
+{
+    public class Camera : MonoBehaviour
+    {
+        [SerializeField] Transform player;
+        [SerializeField] float smoothing;
+        [SerializeField] Vector3 offset;
+        void Update() // Camera that follows the player
+        {
+            if (player != null)
+            {
+                Vector3 newPosition = Vector3.Lerp(transform.position, player.transform.position + offset, smoothing);
+                transform.position = newPosition;
+
+            }
+
+        }
+    }
+
+}
