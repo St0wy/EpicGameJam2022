c258e9777b1e5e40f222d568658b09535193bc3c
Stowy <stowy@protonmail.ch>
2022-07-01 22:27:45 +0200

    modified health script to to the overheal check in a property

--- Assets/_Project/Scripts/Health.cs
+++ Assets/_Project/Scripts/Health.cs
@@ -4,54 +4,45 @@ using UnityEngine;
 
 namespace EpicGameJam
 {
-    public class Health : MonoBehaviour
-    {
-	    public static Health health_;
-       public delegate void HurtCallback(int healthPoints);
+	public class Health : MonoBehaviour
+	{
+		public delegate void HurtCallback(int healthPoints);
 
-		
 		[SerializeField] private int maxHealthPoints = 10;
-		[SerializeField] private int potionHeal= 2;
-
+		[SerializeField] private int potionHeal = 2;
 		[SerializeField] private bool destroyWhenKilled = true;
-
-		
 		[SerializeField] private float destroyTime;
-
+		
+		private int healthPoints;
 
 		public HurtCallback OnHurt { get; set; }
 		public bool IsAlive { get; private set; }
-		public int HealthPoints { get; private set; }
-		
-		
+
+		public int HealthPoints
+		{
+			get => healthPoints;
+			private set
+			{
+				healthPoints = value;
+				if (healthPoints > maxHealthPoints)
+					healthPoints = maxHealthPoints;
+			}
+		}
+
 		private void Awake()
 		{
 			HealthPoints = maxHealthPoints;
 			IsAlive = true;
 		}
 
-		private void Update()
-		{
-			
-		}
-		
 		public void HealCharacter(int heal)
 		{
-			
 			HealthPoints += heal;
-			CheckOverheal();
 		}
-		private void CheckOverheal()
+
+		public void ReduceHealth(int amount)
 		{
-			if (HealthPoints > maxHealthPoints)
-			{
-				HealthPoints = maxHealthPoints;
-			}
-		}
-		
-		public void ReduceHealth(int ammount)
-		{
-			HealthPoints -= ammount;
+			HealthPoints -= amount;
 
 			if (HealthPoints <= 0)
 			{
@@ -60,12 +51,10 @@ namespace EpicGameJam
 
 			OnHurt?.Invoke(HealthPoints);
 
-
 			if (!IsAlive && destroyWhenKilled)
 			{
 				Destroy(gameObject, destroyTime);
 			}
 		}
 	}
-    
 }
