277b8b2d0306afdffd53495c867b6fa38337f794
Florian Rossignol <rossignol.florian.fr@gmail.com>
2022-07-01 23:44:56 +0200

    work on prefab and script

--- Assets/_Project/Scripts/PeonScript.cs
+++ Assets/_Project/Scripts/PeonScript.cs
@@ -1,35 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace EpicGameJam
 {
     public class PeonScript : MonoBehaviour
     {
-        [SerializeField] private float life_ = 10.0f;
-        [SerializeField] private float speed_ = 2.0f;
-        [SerializeField] private Rigidbody2D playerRigidbody2d_;
-        private Vector2 movement_;
+        [FormerlySerializedAs("life_")] [SerializeField]
+        private float _life = 10.0f;
+
+        [FormerlySerializedAs("speed_")] [SerializeField]
+        private float _speed = 2;
         
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
+        [FormerlySerializedAs("bodyPeon_")] [FormerlySerializedAs("playerRigidbody2d_")] [SerializeField]
+        private Rigidbody2D _bodyPeon;
 
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+        [FormerlySerializedAs("bodyPlayer_")] [SerializeField]
+        private Rigidbody2D _bodyPlayer;
 
-        private void CalculatePath(Rigidbody2D bodyPlayer)
+        [SerializeField] private float distanceToActivate = 10;
+        [SerializeField] private Transform playerTrans;
+        private Vector2 movement_;
+
+        private void Update()
         {
-            if (bodyPlayer.CompareTag("Player"))
-            {
-                movement_.x *= speed_;
-                movement_.y *= speed_;
-            }
+            Vector2 peonToTarget = playerTrans.position - transform.position;
+
+            if (!(peonToTarget.magnitude < distanceToActivate)) return;
+            
+            var movementVec = peonToTarget.normalized * _speed;
+            transform.Translate(movementVec);
+            // TODO: replace translate by RigidBody
         }
     }
 }
