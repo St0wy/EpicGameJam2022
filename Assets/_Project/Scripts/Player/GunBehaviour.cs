b5dfbcd88c5c00c4f1ce9b7906476fe1e27f94ef
Stowy <stowy@protonmail.ch>
2022-07-02 17:14:27 +0200

    added gun around player

---
+++ Assets/_Project/Scripts/Player/GunBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace EpicGameJam.Player
+{
+	[RequireComponent(typeof(PlayerMovement))]
+	public class GunBehaviour : MonoBehaviour
+	{
+		[SerializeField] private Transform gunPivot;
+
+		private PlayerMovement _pm;
+
+		private void Awake()
+		{
+			_pm = GetComponent<PlayerMovement>();
+		}
+
+		private void Update()
+		{
+			gunPivot.transform.up = _pm.LookDir;
+		}
+	}
+}
