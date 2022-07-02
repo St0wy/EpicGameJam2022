9c25d4c2171501c9550acebbd69035ef955b0f08
André <a.silva14@hotmail.com>
2022-07-02 10:09:55 +0200

    Sprites and animation started

---
+++ Assets/_Project/Scripts/Player/Animation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EpicGameJam
+{
+    public class Animation : MonoBehaviour
+    {
+        
+        [SerializeField] private Animator swordAnimator;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
